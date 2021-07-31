addced4de444ea078c83d279d69b17ad6fdf9245
Andrew Engleman <drew9781@yahoo.com>
2021-07-31 01:12:29 -0500

    add

---
+++ drewsTools/l1/proxmoxFunctions.py
@@ -0,0 +1,59 @@
+from drewsTools.l0 import proxmox
+
+def migrateVm(vm, allowedNodes=[]):
+    candidateList = getNodeCandidateList(allowedNodes)
+    
+    for nodeName in candidateList['candidateList']:
+        print('filler')
+        #validate candidate
+
+    # if no space evacuate some not-needed VMs
+
+# returns node stats and candidate list
+# {
+#     'nodeName' :{
+#         'availablecpu': 7.997061127069271, 
+#         'maxcpu': 8, 
+#         'percentFreecpu': 0.9996326408836589, 
+#         'availablemem': 15209676800, 
+#         'maxmem': 16649740288,
+#         'percentFreemem': 0.913508351296152, 
+#         'availabledisk': 25744662528, 
+#         'maxdisk': 29194506240, 
+#         'percentFreedisk': 0.8818324350602205, 
+#         'averageFreePercent': 0.9316578090800105
+#     },
+#     'candidateList' : ['mostFreeNode', 'notAsFreeNode']
+# }
+def getNodeCandidateList(allowedNodes=[]):
+    Proxmox=proxmox.Proxmox()
+
+    nodeStats={}
+    for node in Proxmox.getNodes():
+        if node['node'] not in allowedNodes:
+            continue
+            
+        sysType=['cpu','mem','disk']
+        percentFreeList=[]
+        nodeStats[node['node']] = {}
+        for type in sysType:
+            nodeStats[node['node']]['available'+type]  = node['max'+type]  - node[type]
+            nodeStats[node['node']]['max'+type]  = node['max'+type]
+            nodeStats[node['node']]['percentFree'+type]  = (node['max'+type] - node[type]) / node['max'+type]
+            percentFreeList.append( nodeStats[node['node']]['percentFree'+type] )
+        
+        nodeStats[node['node']]['averageFreePercent'] = sum(percentFreeList) / len(percentFreeList)
+    
+    # Sort candidate list with most Free resources first
+    candidateList= list(nodeStats.keys())
+    swapped=True
+    while swapped:
+        swapped=False
+        for i in range(len(candidateList) - 1):
+            if nodeStats[ candidateList[i] ]['averageFreePercent'] < nodeStats[ candidateList[i + 1] ]['averageFreePercent']:
+                candidateList[i], candidateList[i+1] = candidateList[i+1], candidateList[i]
+                swapped=True
+
+    nodeStats['candidateList'] = candidateList
+    return nodeStats 
+
--- scripts/scaleDownPve.py
+++ scripts/scaleDownPve.py
@@ -3,17 +3,56 @@
 
 from drewsTools.l0 import proxmox
 from drewsTools.l0 import functions
+from drewsTools.l1 import proxmoxFunctions
+import time
 
-# Get desired stat conf
-conf=functions.readFile( filetype="yaml", filename="config/pveLowState.yml")
 
-# shutdown everything but core VMs
-Proxmox=proxmox.Proxmox()
-for vm in Proxmox.getVms():
-    if vm['template'] != 1 and vm['status'] == 'running' and vm['name'] not in conf['vms']:
-        print("powering down %s" % vm['name'])
-        # status=Proxmox.setVmPowerStatus(vm['node'], vm['vmid'], 'shutdown')
+def readConf():
+    return functions.readFile( filetype="yaml", filename="config/pveLowState.yml")
 
-# Migrate all Vms to nodes to keep on
+def shutdownNotNeededVms(Proxmox, conf):
+    done=False
+    iteration=0
+    while not done:
+        print("Doing iteration %s" % iteration)
+        iteration += 1
+        running=False
+        for vm in Proxmox.getVms():
+            if vm['template'] != 1 and vm['status'] == 'running' and vm['name'] not in conf['vms']:
+                running=True
+                print("powering down %s" % vm['name'])
+                status=Proxmox.setVmPowerStatus(vm['node'], vm['vmid'], 'shutdown')
+        if not running:
+            done=True
+        else:
+            time.sleep(30)
+
+def migrateVmsToGoodNodes(Proxmox, conf):
+    # Migrate all Vms to nodes to keep on
+    toMigrateList=[]
+    # getVms to migrate
+    for vm in Proxmox.getVms():
+        if vm['name'] not in conf['vms'] or vm['node']  in conf['nodes']:
+            continue
+        else:
+            toMigrateList.append(vm)
+
+    for vm in toMigrateList:
+        proxmoxFunctions.migrateVm(vm, conf['nodes'])
+    # migrate VM
+        # evaluate node usage
+        # pick lower used node by % 
+        # if no space evacuate some not-needed VMs
+
+
 
 # Shutdown nodes
+
+def main():
+    conf=readConf()
+    Proxmox=proxmox.Proxmox()
+
+    shutdownNotNeededVms(Proxmox, conf)
+    migrateVmsToGoodNodes(Proxmox,conf)
+
+main()
